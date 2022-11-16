f6bc8e91f87ac20bfe77278d654bba9875fdb7e8
JesseTheRobot <jesse.cruz.wright@gmail.com>
2022-11-16 19:17:05 +0000

    feat: :sparkles: Worker signing

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -1,15 +1,9 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `wrangler dev src/index.ts` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `wrangler publish src/index.ts --name my-worker` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
-//@ts-ignore
-import test from "testmodule";
+import "node-window-polyfill/register";
+import { SolanaBundlr } from "@bundlr-network/solana-web";
+import { sign } from "@noble/ed25519";
+import bs58 from "bs58";
+import fetchAdapter from "@vespaiach/axios-fetch-adapter";
+import { setIsBrowser } from "@bundlr-network/client/common";
 
 export interface Env {
 	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
@@ -28,7 +22,42 @@ export default {
 		env: Env,
 		ctx: ExecutionContext
 	): Promise<Response> {
-		test(); // logs "browser" 
-		return new Response("Hello World!");
+		try {
+			// these can be pre-derived and hardcoded as an env var. (encode as hex string, store in .env, decode here.)
+			const key = bs58.decode("...");
+			const priv = key.subarray(0, 32);
+			const pub = key.subarray(32, 64);
+
+			const providerShim = {
+				publicKey: {
+					toBuffer: () => pub,
+					byteLength: 32
+				},
+				signMessage: async (message: Uint8Array) => {
+					return await sign(Buffer.from(message), priv);
+				},
+
+			};
+
+			const nodeUrl = "https://devnet.bundlr.network";
+			setIsBrowser(true);
+			//@ts-ignore
+			const b = new SolanaBundlr(nodeUrl, providerShim, { api: { adapter: fetchAdapter } });
+			await b.ready();
+
+			const tx = b.createTransaction("Hello world!");
+			await tx.sign();
+			console.log("Tx is valid?", await tx.isValid());
+
+			const res = await tx.upload();
+
+			console.log(JSON.stringify(res));
+
+			return new Response(JSON.stringify(res));
+		} catch (e) {
+			console.log(e);
+			console.log("ERROR", JSON.stringify(e));
+			return new Response("");
+		}
 	},
 };
